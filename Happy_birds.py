a878a9a34c9cac6d1bf9a9fee7a8e4825da00d0b
Maxime <maximegielly05@gmail.com>
2021-02-04 12:32:28 +0100

    new : modified long of line according to PEP8 (80 words max)

--- Happy_birds.py
+++ Happy_birds.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import tkinter as tk
-import math
+import math as m
 import matplotlib.pyplot as plt 
 #from datetime import datetime, timedelta 
 #import time
@@ -25,14 +25,21 @@ class App_happy_birds(tk.Tk):
 		# intervalle de temps entre 2 points
 		self.dt = 0.004
 		# Création et packing du canvas
-		self.canv = tk.Canvas(self, bg='light gray', height=self.height_fenetre, width=self.widht_fenetre, highlightbackground="black")
+		self.canv = tk.Canvas(self, bg='light gray', height=self.height_fenetre,
+							  width=self.widht_fenetre, 
+							  highlightbackground="black")
 		self.canv.pack(side=tk.LEFT)
 		# Création du projectile
-		self.project = self.canv.create_oval(self.x_center-self.diam/2, self.y_center-self.diam/2,
-							 self.x_center+self.diam/2, self.y_center+self.diam/2, fill="black")
+		self.project = self.canv.create_oval(self.x_center-self.diam/2,
+											 self.y_center-self.diam/2,
+											 self.x_center+self.diam/2,
+											 self.y_center+self.diam/2,
+											 fill="black")
 		# création des axes
-		self.canv.create_line(self.x_center, 0, self.x_center, self.height_fenetre, fill="red", dash=(4, 4))
-		self.canv.create_line(0, self.y_center, self.widht_fenetre, self.y_center, fill="red", dash=(4, 4))
+		self.canv.create_line(self.x_center, 0, self.x_center,
+							  self.height_fenetre, fill="red", dash=(4, 4))
+		self.canv.create_line(0, self.y_center, self.widht_fenetre,
+							  self.y_center, fill="red", dash=(4, 4))
 		
 		self.canv.bind("<Button -1>", self.click)
 		self.bind("<space>", self.move)
@@ -41,42 +48,57 @@ class App_happy_birds(tk.Tk):
 		# compteur pour les points de suivis
 		self.delay_pnt = 0
 
-		# Initialisation de la vairable représentant la boucle "after" de la fonction move()
+		# Initialisation de la vairable représentant 
+		# la boucle "after" de la fonction move()
 		self.solve = 0
 		
 		# Create widgets
 		self.creat_widgets()
 
 	def creat_widgets(self):
-	    self.bouton_restart = tk.Button(self, text=" Restart ", command=self.restart)
+	    self.bouton_restart = tk.Button(self, text=" Restart ",
+	    								command=self.restart)
 	    self.bouton_restart.pack(side=tk.TOP)
-	    self.bouton_quit = tk.Button(self, text=" QUIT ", command=self.quitte)
+	    self.bouton_quit = tk.Button(self, text=" QUIT ",
+	    							 command=self.quitte)
 	    self.bouton_quit.pack(side=tk.BOTTOM)
 
 	def click(self, mclick):
-		if mclick.x>self.x_center: # si l'utilisateur place le projectile après l'absisse du lanceur
+		# si l'utilisateur place le projectile après l'absisse du lanceur
+		if mclick.x>self.x_center: 
 			print("ERROR POSITION")
 		else:
-			if self.solve != 0: # si un lancé est toujours en cours (boucle sur la fc move) on l'arrete
+			# si un lancé est toujours en cours (boucle move), on l'arrete
+			if self.solve != 0: 
 				self.after_cancel(self.solve)
-
-			if self.first == False: # Si ce n'est pas le premier lancé effacer l'ancienne trajectoire et projectile
+			# Si ce n'est pas le premier lancé 
+			# effacer l'ancienne trajectoire et l'ancien projectile
+			if self.first == False: 
 				self.canv.delete(self.project)
 				self.canv.delete(self.traj)
 			self.first = False
 
 			self.x0 = mclick.x
 			self.y0 = mclick.y
-			# Initialisation de l'angle et de la norme du vecteur (click souris - centre du lanceur)
-			self.beta = math.atan((self.y_center-self.y0)/(self.x_center-self.x0))
-			self.L = math.sqrt((self.x0-self.x_center)**2 + (self.y0-self.y_center)**2)
+			# Initialisation du vecteur (click souris - centre du lanceur)
+			nominater = self.y_center-self.y0
+			denominateur = self.x_center-self.x0
+			self.beta = m.atan(nominater/denominateur)
+			self.L = m.sqrt((self.x0-self.x_center)**2 
+								+ (self.y0-self.y_center)**2)
 			# Création du projectile à la position initiale (click souris)
-			self.project = self.canv.create_oval(mclick.x-self.diam, mclick.y-self.diam,
-								 mclick.x+self.diam, mclick.y+self.diam, fill="black")
+			self.project = self.canv.create_oval(mclick.x-self.diam,
+												 mclick.y-self.diam,
+												 mclick.x+self.diam,
+												 mclick.y+self.diam,
+												 fill="black")
 			# Initialisation du vecteur d'indicateur de direction du lancé
-			self.traj = self.canv.create_line(mclick.x, mclick.y, 
-						mclick.x+1.5*self.L*math.cos(self.beta), 
-						mclick.y+1.5*self.L*math.sin(self.beta), arrow='last',  fill="green", dash=(4, 4))
+			x_final = mclick.x+1.5*self.L*m.cos(self.beta)
+			y_final = mclick.y+1.5*self.L*m.sin(self.beta)
+			self.traj = self.canv.create_line(mclick.x, mclick.y,
+											  x_final, y_final,
+											  arrow='last', fill="green",
+											  dash=(4, 4))
 
 			# Initialisation des positions, vitesse et temps
 			self.x = self.x0
@@ -86,22 +108,28 @@ class App_happy_birds(tk.Tk):
 			self.t = 0
 
 	def move(self, event=None):
-		# MAJ de l'angle et de la norme du vecteur (projectile - centre du lanceur)
-		self.beta = math.atan((self.y_center-self.y)/(self.x_center-self.x))
-		self.L = math.sqrt((self.x-self.x_center)**2 + (self.y-self.y_center)**2)
+		# MAJ de l'angle + norme du vecteur (projectile-centre du lanceur)
+		self.beta = m.atan((self.y_center-self.y)/(self.x_center-self.x))
+		self.L = m.sqrt((self.x-self.x_center)**2 + (self.y-self.y_center)**2)
 		
-		# Lorsque le prjectile est propulsé par le lanceur la force de rappel du lanceur est non nulle
+		# Lorsque le prjectile est propulsé par le lanceur
+		# la force de rappel du lanceur est non nulle
 		if self.x<self.x_center: 
 			self.force_rappel = self.coef_rappel*self.L
 		else : 
 			self.force_rappel = 0
 
 		self.poids = 1/2*self.g
-		# Calcule de l'accélération, de la vitesse pouis de la position du projectile
-		self.acceleration_x = self.force_rappel*math.cos(self.beta) 
-		self.acceleration_y = self.force_rappel*math.sin(self.beta) + self.poids
-		self.vitesse_x = self.acceleration_x*self.t + self.vitesse_x - self.coef_frottements*self.vitesse_x**2
-		self.vitesse_y = self.acceleration_y*self.t + self.vitesse_y + self.coef_frottements*self.vitesse_y**2
+		# Calcule de l'accélé, de la vitesse puis de la position du projectile
+		self.acceleration_x = self.force_rappel*m.cos(self.beta) 
+		self.acceleration_y = self.force_rappel*m.sin(self.beta) + self.poids
+
+		self.vitesse_x = self.acceleration_x*self.t + self.vitesse_x - \
+						 self.coef_frottements*self.vitesse_x**2
+
+		self.vitesse_y = self.acceleration_y*self.t + self.vitesse_y + \
+						 self.coef_frottements*self.vitesse_y**2
+
 		self.x = self.vitesse_x*self.t + self.x
 		self.y = self.vitesse_y*self.t + self.y
 		# MAJ de la position du projectile
@@ -114,7 +142,8 @@ class App_happy_birds(tk.Tk):
 		self.pnt = self.canv.create_oval(self.x, self.y,
 		 		self.x+2, self.y+2, fill="blue", width=1)
 
-		# Stopper le mouvement du projectile lorsqu'il disparait verticalement de l'écran
+		# supprime le mouvement du projectile lorsqu'il disparait 
+		# verticalement de l'écran
 		if (self.y-10*self.diam) > self.height_fenetre:
 			self.canv.delete(self.project)
 			self.vitesse_x = 0
@@ -126,7 +155,7 @@ class App_happy_birds(tk.Tk):
 
 	def restart(self):
 		self.first = True
-		if self.solve != 0 : # Stop la boucle "after" de la fc move si elle est lancée
+		if self.solve != 0 : # Stop la boucle "after" si elle est lancée
 			self.after_cancel(self.solve)
 		self.canv.delete("all")
 		self.vitesse_x = 0
@@ -134,11 +163,16 @@ class App_happy_birds(tk.Tk):
 		self.t = 0
 
 		# création des axes
-		self.canv.create_line(self.x_center, 0, self.x_center, self.height_fenetre, fill="red", dash=(4, 4))
-		self.canv.create_line(0, self.y_center, self.widht_fenetre, self.y_center, fill="red", dash=(4, 4))
+		self.canv.create_line(self.x_center, 0, self.x_center,
+							  self.height_fenetre, fill="red", dash=(4, 4))
+		self.canv.create_line(0, self.y_center, self.widht_fenetre,
+							 self.y_center, fill="red", dash=(4, 4))
 		# Création du projectile
-		self.project = self.canv.create_oval(self.x_center-self.diam/2, self.y_center-self.diam/2,
-							 self.x_center+self.diam/2, self.y_center+self.diam/2, fill="black")
+		self.project = self.canv.create_oval(self.x_center-self.diam/2,
+											 self.y_center-self.diam/2,
+											 self.x_center+self.diam/2,
+											 self.y_center+self.diam/2,
+											 fill="black")
 
 	def quitte(self):
 		self.canv.delete("all")
