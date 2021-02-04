d781f4f2ea8f10e7f5b889bd3b7c8d93f33455a0
Maxime <maximegielly05@gmail.com>
2021-02-04 12:06:00 +0100

    new : add comment

--- Happy_birds.py
+++ Happy_birds.py
@@ -3,8 +3,8 @@
 import tkinter as tk
 import math
 import matplotlib.pyplot as plt 
-from datetime import datetime, timedelta 
-import time
+#from datetime import datetime, timedelta 
+#import time
 
 class App_happy_birds(tk.Tk):
 	def __init__(self):
@@ -13,7 +13,7 @@ class App_happy_birds(tk.Tk):
 		# gravite
 		self.g = 9.8
 		self.coef_rappel = 0.5
-		self.coef_frottements = 0
+		self.coef_frottements = 0.0002
 		self.widht_fenetre = 1600
 		self.height_fenetre = 800
 		self.x_center = 200
@@ -48,35 +48,37 @@ class App_happy_birds(tk.Tk):
 		self.creat_widgets()
 
 	def creat_widgets(self):
-	    #self.bouton_start = tk.Button(self, text=" Start ", command=self.move)
-	    #self.bouton_start.pack(side=tk.TOP)
 	    self.bouton_restart = tk.Button(self, text=" Restart ", command=self.restart)
 	    self.bouton_restart.pack(side=tk.TOP)
 	    self.bouton_quit = tk.Button(self, text=" QUIT ", command=self.quitte)
 	    self.bouton_quit.pack(side=tk.BOTTOM)
 
 	def click(self, mclick):
-		if mclick.x>self.x_center:
+		if mclick.x>self.x_center: # si l'utilisateur place le projectile après l'absisse du lanceur
 			print("ERROR POSITION")
 		else:
-			if self.solve != 0:
+			if self.solve != 0: # si un lancé est toujours en cours (boucle sur la fc move) on l'arrete
 				self.after_cancel(self.solve)
 
-			if self.first == False:
+			if self.first == False: # Si ce n'est pas le premier lancé effacer l'ancienne trajectoire et projectile
 				self.canv.delete(self.project)
 				self.canv.delete(self.traj)
 			self.first = False
 
 			self.x0 = mclick.x
 			self.y0 = mclick.y
+			# Initialisation de l'angle et de la norme du vecteur (click souris - centre du lanceur)
 			self.beta = math.atan((self.y_center-self.y0)/(self.x_center-self.x0))
 			self.L = math.sqrt((self.x0-self.x_center)**2 + (self.y0-self.y_center)**2)
+			# Création du projectile à la position initiale (click souris)
 			self.project = self.canv.create_oval(mclick.x-self.diam, mclick.y-self.diam,
 								 mclick.x+self.diam, mclick.y+self.diam, fill="black")
+			# Initialisation du vecteur d'indicateur de direction du lancé
 			self.traj = self.canv.create_line(mclick.x, mclick.y, 
 						mclick.x+1.5*self.L*math.cos(self.beta), 
 						mclick.y+1.5*self.L*math.sin(self.beta), arrow='last',  fill="green", dash=(4, 4))
 
+			# Initialisation des positions, vitesse et temps
 			self.x = self.x0
 			self.y = self.y0
 			self.vitesse_x = 0
@@ -84,43 +86,47 @@ class App_happy_birds(tk.Tk):
 			self.t = 0
 
 	def move(self, event=None):
+		# MAJ de l'angle et de la norme du vecteur (projectile - centre du lanceur)
 		self.beta = math.atan((self.y_center-self.y)/(self.x_center-self.x))
 		self.L = math.sqrt((self.x-self.x_center)**2 + (self.y-self.y_center)**2)
-
-		if self.x<self.x_center:
+		
+		# Lorsque le prjectile est propulsé par le lanceur la force de rappel du lanceur est non nulle
+		if self.x<self.x_center: 
 			self.force_rappel = self.coef_rappel*self.L
 		else : 
 			self.force_rappel = 0
-			self.coef_frottements = 0.0002
+
 		self.poids = 1/2*self.g
+		# Calcule de l'accélération, de la vitesse pouis de la position du projectile
 		self.acceleration_x = self.force_rappel*math.cos(self.beta) 
 		self.acceleration_y = self.force_rappel*math.sin(self.beta) + self.poids
 		self.vitesse_x = self.acceleration_x*self.t + self.vitesse_x - self.coef_frottements*self.vitesse_x**2
 		self.vitesse_y = self.acceleration_y*self.t + self.vitesse_y + self.coef_frottements*self.vitesse_y**2
 		self.x = self.vitesse_x*self.t + self.x
 		self.y = self.vitesse_y*self.t + self.y
+		# MAJ de la position du projectile
 		self.canv.coords(self.project, self.x-self.diam, self.y-self.diam,
 	 					self.x+self.diam, self.y+self.diam)
-		self.t += self.dt
+		self.t += self.dt # incrémentation du temps
 
-		#if self.delay_pnt == 5:
-			# mise à jour des points de suivis
+
+		# mise à jour des points de suivis du projectile
 		self.pnt = self.canv.create_oval(self.x, self.y,
 		 		self.x+2, self.y+2, fill="blue", width=1)
-			#self.delay_pnt = 0
-		#self.delay_pnt+=1
+
+		# Stopper le mouvement du projectile lorsqu'il disparait verticalement de l'écran
 		if (self.y-10*self.diam) > self.height_fenetre:
 			self.canv.delete(self.project)
 			self.vitesse_x = 0
 			self.vitesse_y = 0
 			self.t = 0
 
-		self.solve = self.after(15, self.move)
+		self.solve = self.after(15, self.move) # boucle sur la fonction move
 
 
 	def restart(self):
 		self.first = True
-		if self.solve != 0 :
+		if self.solve != 0 : # Stop la boucle "after" de la fc move si elle est lancée
 			self.after_cancel(self.solve)
 		self.canv.delete("all")
 		self.vitesse_x = 0
@@ -137,7 +143,6 @@ class App_happy_birds(tk.Tk):
 	def quitte(self):
 		self.canv.delete("all")
 		self.destroy()
-		#self.after_cancel(self.solve)
 
 if __name__ == "__main__":
 	app = App_happy_birds()
